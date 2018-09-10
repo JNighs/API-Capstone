8ce6cdc0da0cd721253340bb1890a769b0a47b53
James Nighswonger <james.nighswonger@gmail.com>
2018-09-10 11:53:17 -0700

    commit before Flickity

--- index.js
+++ index.js
@@ -1,24 +1,61 @@
 const OMDb_SEARCH_URL = 'https://www.omdbapi.com/';
-const TMDb_SEARCH_URL = 'https://api.themoviedb.org/3/find/';
+const TMDb_SEARCH_URL = 'https://api.themoviedb.org/3/search/movie';
+const TMDb_NOWPLAYING_URL = 'https://api.themoviedb.org/3/movie/now_playing'
+const TMDb_POPULAR_URL = 'https://api.themoviedb.org/3/movie/popular'
+const TMDb_TOPRATED_URL = 'https://api.themoviedb.org/3/movie/top_rated'
+const TMDb_UPCOMING_URL = 'https://api.themoviedb.org/3/movie/upcoming'
 const TMDb_MOVIE_URL = 'https://api.themoviedb.org/3/movie/'
-const TMDb_IMAGE_URL = 'https://image.tmdb.org/t/p/original/';
+const TMDb_DISCOVER_URL = 'https://aple.themoviedb.org/3/discover/movie'
+const TMDb_IMAGE_BIG_URL = 'https://image.tmdb.org/t/p/original/';
+const TMDb_IMAGE_SMALL_URL = 'https://image.tmdb.org/t/p/w500/';
 const YOUTUBE_VIDEO_URL = 'https://www.youtube.com/watch?v=';
 const YOUTUBE_THUMBNAIL_URL = 'https://img.youtube.com/vi/';
 const searchObj = {};
-
-function searchFromOMDB() {
-  const query = {
-    s: `${searchObj.value}`,
-    apikey: 'c6c932dc',
-    type: 'movie',
-    page: 1,
-  }
-  $.getJSON(OMDb_SEARCH_URL, query, displaySearchResults);
+const key = {
+  TMDb: '46922a4eb88a052d565922dfe0666828',
+  OMDb: 'c6c932dc'
 }
 
-function findIDs(resultID) {
-  searchObj.imdbID = searchObj.results.Search[resultID].imdbID;
-  findIDFromTMDb();
+function searchTMDb(search) {
+  const query = {
+    api_key: key.TMDb,
+    query: search
+  }
+  $.getJSON(TMDb_SEARCH_URL, query, displayResults);
+}
+
+function discoverSearch(discover, country) {
+  const query = {
+    api_key: key.TMDb,
+    region: country
+  }
+  let discoverURL = '';
+  switch (discover) {
+    case 'nowPlaying':
+      discoverURL = TMDb_NOWPLAYING_URL;
+      break;
+    case 'popular':
+      discoverURL = TMDb_POPULAR_URL;
+      break;
+    case 'topRated':
+      discoverURL = TMDb_TOPRATED_URL;
+      break;
+    case 'upcoming':
+      discoverURL = TMDb_UPCOMING_URL;
+      break;
+  }
+
+  $.getJSON(discoverURL, query, displayResults);
+}
+
+function displayResults(data) {
+  //Filter results to not show any films that don't have a movie poster.
+  const results = data.results.filter(movie => movie.poster_path);
+  console.log(results);
+  const printResults = results.map((item, index) => renderResult(item, index));
+  $('.js-search-results').html(printResults);
+  $('.js-search-results').removeClass('hidden');
+
 }
 
 function getDataFromOMDb() {
@@ -29,27 +66,11 @@ function getDataFromOMDb() {
   return $.getJSON(OMDb_SEARCH_URL, query);
 }
 
-function getDataFromTMDb() {
-  const query = {
-    api_key: '46922a4eb88a052d565922dfe0666828'
-  }
-  return $.getJSON(`${TMDb_MOVIE_URL}${searchObj.TMDbID}`, query)
-}
-
 function getVideoFromTMDb() {
   const query = {
     api_key: '46922a4eb88a052d565922dfe0666828'
   }
   return $.getJSON(`${TMDb_MOVIE_URL}${searchObj.TMDbID}/videos`, query)
-}
-
-function findIDFromTMDb() {
-  const query = {
-    api_key: '46922a4eb88a052d565922dfe0666828',
-    language: 'en-US',
-    external_source: 'imdb_id'
-  }
-  return $.getJSON(`${TMDb_SEARCH_URL}${searchObj.imdbID}`, query, logTMDbID);
 }
 
 function logTMDbID(data) {
@@ -65,16 +86,12 @@ function logTMDbID(data) {
 function renderResult(result, index) {
   return `
     <div class="movie-result col span_2_of_10">
-        <img class="result-image" src="${result.Poster}" id="${index}" alt="REPLACE">
+        <img class="result-image" src="${TMDb_IMAGE_SMALL_URL}${result.poster_path}" id="${index}" alt="REPLACE">
       <h3>
-        <a class="js-user-name" href="https://www.imdb.com/title/${result.imdbID}/" target="_blank">${result.Title}</a>
+        <a class="js-user-name" href="" target="_blank">${result.original_title}</a>
       </h3>
     </div>
   `;
-}
-
-function showResultPage(data) {
-  displaySearchResults(data);
 }
 
 function displayMovieData() {
@@ -125,6 +142,7 @@ function displayResultsText(data) {
 
 function displaySearchResults(data) {
   searchObj.results = data;
+  console.log(data);
   const results = data.Search.map((item, index) => renderResult(item, index));
   $('.js-search-results').html(results);
   $('.js-search-results').removeClass('hidden');
@@ -138,14 +156,21 @@ function watchSubmit() {
     // clear out the input
     queryTarget.val("");
     $('.movie-container').addClass('hidden');
-    searchFromOMDB();
+    searchTMDb(searchObj.value);
   });
 }
 
 function watchMovieClick() {
-  $('.js-search-results').on("click", ".result-image", function () {
-    findIDs(this.id);
-  });
+  $('.js-search-results').on("click", ".result-image", displayMovieDetails);
+}
+
+function displayMovieDetails(e) {
+  $('.js-search-results').before('<div class="movieDetails section group"></div>');
+  $('.movieDetails').animate({
+    height: "500px"
+  }, 500, 'linear', function() {
+
+  })
 }
 
 function watchContainerClose() {
@@ -154,10 +179,33 @@ function watchContainerClose() {
   })
 }
 
+function watchNowPlaying() {
+  $('.js-now-playing').submit(event => {
+    const discover = $(event.currentTarget).find('.discover').val();
+    const country = $(event.currentTarget).find('.country').val();
+    event.preventDefault();
+    discoverSearch(discover, country);
+  })
+}
+
+function listCountries() {
+  const dropDown = $('.country');
+  countryDb.forEach(country => {
+    dropDown.append($('<option></option>').attr('value', country.iso_3166_1).text(country.english_name))
+  })
+  //Make US the default value
+  $("option[value='US']").prop('selected', true);
+}
+
 function onLoad() {
-  watchSubmit();
-  watchMovieClick();
+  /*
   watchContainerClose();
+  */
+
+  watchMovieClick();
+  watchSubmit();
+  watchNowPlaying();
+  listCountries();
 }
 
 $(onLoad);
